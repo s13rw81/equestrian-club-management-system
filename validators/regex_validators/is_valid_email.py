51faa779f5f90a12bf83d0dca8ccd43d58a2870a
Nilanjan <nilthedev@gmail.com>
2024-06-09 16:59:26 +0530

    added docstring in `is_valid_email`

--- validators/regex_validators/is_valid_email.py
+++ validators/regex_validators/is_valid_email.py
@@ -3,6 +3,13 @@ from logging_config import log
 
 
 def is_valid_email(email: str) -> bool:
+
+    """
+    returns True if it's a valid email address, false otherwise
+    :param email: the string to be checked
+    :return: True if it's a valid email address, false otherwise
+    """
+
     log.info(f"inside is_valid_email(email={email})")
     email_regex = re.compile(
         r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"

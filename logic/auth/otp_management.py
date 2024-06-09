05e1177a6011a90682dfe7462bafcaf2918d5db8
Nilanjan <nilthedev@gmail.com>
2024-06-10 02:15:43 +0530

    refactored `otp-management` code

--- logic/auth/otp_management.py
+++ logic/auth/otp_management.py
@@ -1,58 +1,61 @@
 from fastapi import status
+from typing import Optional
+from validators.regex_validators import is_valid_email
 from fastapi.exceptions import HTTPException
 from data.dbapis.user.write_queries import update_user
 from data.dbapis.user.read_queries import get_user_by_email
-from models.user import UpdateUserInternal, EmailVerificationOTP
-from external_services.otp_service import send_otp
+from models.user import UpdateUserInternal, SignUpVerificationOTP, UserInternal
+from models.user.enums import SignUpCredentialType
+from external_services.otp_service import send_otp_email, send_otp_phone
 from logging_config import log
 
 
-def send_email_verification_otp_logic(email_address: str) -> bool:
+def send_sign_up_otp(user: UserInternal) -> bool:
     """
-        sends otp in the given email address using
+        sends otp in the sign_up credential of the user using
         the corresponding external_service and saves it in the database
 
-        :param email_address: the email_address of the user
+        :param user: the user
         :returns: the result of the database update; a bool
     """
 
     # TODO: integrate the logic that new OTP won't be generated if the old OTP is less than 10 minutes old
 
-    log.info(f"send_otp_logic invoked: email_address={email_address}")
+    log.info(f"inside send_sign_up_otp(user={user})")
 
-    user = get_user_by_email(email=email_address)
+    sent_otp = (send_otp_email(email_address=user.email_address)
+                if user.sign_up_credential_type == SignUpCredentialType.EMAIL_ADDRESS
+                else send_otp_phone(phone_number=user.phone_number))
 
-    sent_otp = send_otp(
-        email_address=email_address,
-        first_name=user.first_name,
-        last_name=user.last_name
-    )
+    update_user_data = UpdateUserInternal(sign_up_verification_otp=SignUpVerificationOTP(otp=sent_otp))
 
-    update_user_data = UpdateUserInternal(email_verification_otp=EmailVerificationOTP(otp=sent_otp))
-
-    result = update_user(update_user_data=update_user_data, email_address=email_address)
+    result = update_user(update_user_data=update_user_data, user=user)
 
     return result
 
 
-def verify_email_verification_otp_logic(email_address: str, user_provided_otp: str) -> bool:
+def verify_sign_up_otp(user: UserInternal, user_provided_otp: str) -> bool:
     """
         matches the provided OTP with the database records for
         the corresponding user; if a match is found the database is updated
         accordingly; returns a bool indicating whether a match was found
 
-        :param email_address: the email_address of the user
+        :param user: UserInternal
         :param user_provided_otp: the user provided OTP
 
         :returns: boolean indicating whether there was a match
     """
 
-    log.info(f"validate_otp invoked: email_address={email_address}, "
-                f"user_provided_otp={user_provided_otp}")
+    log.info(f"verify_sign_up_otp(user={user}, user_provided_otp={user_provided_otp})")
 
-    user = get_user_by_email(email=email_address)
+    if not user.sign_up_verification_otp:
+        log.info("the user hasn't generated OTP, please generate OTP first")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="please generate OTP first"
+        )
 
-    if user.email_verification_otp.otp != user_provided_otp:
+    if user.sign_up_verification_otp.otp != user_provided_otp:
         log.info("the OTPs did not match, raising exception...")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -61,6 +64,6 @@ def verify_email_verification_otp_logic(email_address: str, user_provided_otp: s
 
     update_user_data = UpdateUserInternal(otp_verified=True)
 
-    result = update_user(update_user_data=update_user_data, email_address=email_address)
+    result = update_user(update_user_data=update_user_data, user=user)
 
     return result

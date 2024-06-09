43e352a139ea0dd60c0ce5e696293b1c85fa8eb7
Nilanjan <nilthedev@gmail.com>
2024-06-09 11:50:57 +0530

    implemented `is_valid_email`

---
+++ validators/regex_validators/is_valid_email.py
@@ -0,0 +1,10 @@
+import re
+from logging_config import log
+
+
+def is_valid_email(email: str) -> bool:
+    log.info(f"inside is_valid_email(email={email})")
+    email_regex = re.compile(
+        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    )
+    return re.match(email_regex, email) is not None

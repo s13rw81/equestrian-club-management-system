4a60e2ccc881aad506d4bbbe6dc75bc12a178bf9
Nilanjan <nilthedev@gmail.com>
2024-06-09 11:54:00 +0530

    exposed `user_api_router`

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import uvicorn
 from config import HOST, PORT, DEBUG
 from fastapi import FastAPI, Request
 from logging_config import log
+from api.user import user_api_router
+
 app = FastAPI()
+
+app.include_router(user_api_router)
 
 
 @app.get("/")
 async def root(request: Request):
     log.info(f'method: {request.method}, headers: {request.headers}, client: {request.client}')
     return {"Hello": "World"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=HOST, port=PORT, reload=DEBUG)

3c49ff3915639120b2e0fce49e7a0aad5a63f60a
Nilanjan <nilthedev@gmail.com>
2024-06-10 02:15:18 +0530

    implemented `generate-sign-up-otp` and `verify-sign-up-otp` route

--- api/auth/auth_api.py
+++ api/auth/auth_api.py
@@ -1,13 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Body
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from .models import Token
 from models.user import UserInternal
-# from models.validation_response import ValidationResponse
 from logic.auth import authenticate_user, create_access_token, get_current_user
-from logic.auth.otp_management import send_email_verification_otp_logic, verify_email_verification_otp_logic
+from logic.auth.otp_management import send_sign_up_otp, verify_sign_up_otp
 from logging_config import log
-from validators.user import whether_user_exists
 from validators.regex_validators import is_valid_email
 
 user_auth_router = APIRouter(
@@ -18,7 +16,7 @@ user_auth_router = APIRouter(
 
 @user_auth_router.post("/token")
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
-    log.info(f"/token invoked: username={form_data.username}")
+    log.info(f"inside auth/token username={form_data.username}")
 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -47,3 +45,42 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     log.info(f"returning {retval}")
 
     return retval
+
+
+@user_auth_router.post("/generate-sign-up-otp")
+async def send_otp_for_email_verification(user: Annotated[UserInternal, Depends(get_current_user)]):
+    log.info(f"inside auth/generate-sign-up-otp (user={user})")
+
+    result = send_sign_up_otp(user=user)
+
+    if result:
+        return {"status": "OK"}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="could not send otp"
+        )
+
+
+@user_auth_router.post("/verify-sign-up-otp")
+async def verify_email_verification_otp(
+        user: Annotated[UserInternal, Depends(get_current_user)],
+        user_provided_otp: str
+):
+    log.info(f"auth/verify-sign-up-otp(user={user}, user_provided_otp={user_provided_otp})")
+
+    result = verify_sign_up_otp(
+        user=user,
+        user_provided_otp=user_provided_otp
+    )
+
+    if result:
+        return {"status": "OK"}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="could not send otp"
+        )
+
+
+

ba5657271e243f1812de53be0cf9f234c2a60df8
Sourav B. Roy <wildr.slimshady@gmail.com>
2024-06-25 02:16:33 +0530

    - reset api/auth/models/reset_password_verify.py

--- api/auth/models/reset_password_verify.py
+++ api/auth/models/reset_password_verify.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 
 
 class ResetPasswordVerify(BaseModel):
-    otp: str
+
     email_address: Optional[str] = None
     phone_number: Optional[str] = None
+    otp: str
     new_password: Optional[str]

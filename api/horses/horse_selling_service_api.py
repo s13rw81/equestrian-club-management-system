c4a2574a2b3b3de72148b77df033358f2999d240
mudassir <mudassirkhurshid9@gmail.com>
2024-07-10 15:35:57 +0530

    Added APIs for horse selling service PR#77 and issue #31

---
+++ api/horses/horse_selling_service_api.py
@@ -0,0 +1,253 @@
+from typing import Annotated, List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.requests import Request
+
+from data.dbapis.horse_selling_service.read_queries import (
+    get_all_horse_sell_enquiries,
+    get_horse_sell_listings,
+    get_selling_enquiry_by_user_id,
+)
+from data.dbapis.horse_selling_service.write_queries import (
+    add_horse_selling_service_details,
+    add_horse_selling_service_enquiry,
+    update_horse_selling_service_details,
+    update_horse_selling_service_enquiry,
+    update_selling_service_images,
+)
+from data.dbapis.horses.write_queries import add_horse, update_horse
+from logging_config import log
+from models.horse.horse import HorseInternal, UploadInfo
+from models.horse.horse_selling_service_internal import (
+    HorseSellingServiceEnquiryInternal,
+    HorseSellingServiceInternal,
+    Provider,
+)
+from utils.image_management import generate_image_urls, save_image
+
+from .api_validators.horse_selling_service import (
+    CreateSellEnquiryValidator,
+    EnlistHorseForSellServiceValidator,
+    GetHorseSellEnquiryValidator,
+    GetHorseSellListingValidator,
+    UpdateHorseForSellServiceListingValidator,
+    UpdateSellEnquiryValidator,
+    UploadSellImageValidator,
+)
+from .models import (
+    CreateSellEnquiryResponse,
+    EnlistHorseForSellResponse,
+    GetHorseSellEnquiry,
+    GetHorseSellListing,
+)
+
+horse_selling_service_router = APIRouter(prefix="", tags=["user-horses"])
+
+
+@horse_selling_service_router.post(path="/enlist-for-sell")
+def enlist_horse_for_sell(
+    request: Request,
+    payload: Annotated[EnlistHorseForSellServiceValidator, Depends()],
+) -> EnlistHorseForSellResponse:
+
+    user = payload.user
+    enlist_details = payload.enlist_details
+
+    log.info(f"{request.url.path} invoked enlist_details {enlist_details}")
+
+    provider = UploadInfo(uploaded_by_id=user.id, uploaded_by_user=user.user_role)
+
+    horse = HorseInternal(
+        name=enlist_details.name,
+        year_of_birth=enlist_details.year_of_birth,
+        breed=enlist_details.breed,
+        size=enlist_details.size,
+        gender=enlist_details.gender,
+        description=enlist_details.description,
+        uploaded_by=provider,
+    )
+
+    horse_id = add_horse(horse_details=horse)
+
+    if not horse_id:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unable to enlist horse for rent",
+        )
+
+    provider = Provider(provider_id=user.id, provider_type=user.user_role)
+    selling_service_details = HorseSellingServiceInternal(
+        horse_id=horse_id, provider=provider, price_sar=enlist_details.price
+    )
+
+    selling_service_id = add_horse_selling_service_details(
+        selling_service_details=selling_service_details
+    )
+
+    if not selling_service_id:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unable to enlist horse for sell",
+        )
+
+    response = EnlistHorseForSellResponse(horse_selling_service_id=selling_service_id)
+
+    log.info(f"{request.url.path} returning {response}")
+
+    return response
+
+
+@horse_selling_service_router.post(path="/{horse_selling_service_id}/upload-images")
+async def upload_rent_images(
+    request: Request, payload: Annotated[UploadSellImageValidator, Depends()]
+):
+
+    files = payload.files
+    service_id = payload.horse_selling_service_id
+    log.info(f"{request.url.path} invoked horse_selling_service_id {service_id}")
+
+    image_ids = []
+    for file in files:
+        image_id = await save_image(image_file=file)
+        image_ids.append(image_id)
+
+    if not image_ids:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="unable to save image at this time",
+        )
+
+    update_selling_service_images(service_id=service_id, image_ids=image_ids)
+
+    return {"status": "OK"}
+
+
+@horse_selling_service_router.get(
+    path="/get-horses-for-sell", response_model=List[GetHorseSellListing]
+)
+def get_horse_rent_listing(
+    request: Request, payload: Annotated[GetHorseSellListingValidator, Depends()]
+):
+    user = payload.user
+    own_listing = payload.own_listing
+    log.info(f"{request.url.path} invoked ")
+
+    sell_listings = get_horse_sell_listings(user_id=user.id, own_listing=own_listing)
+
+    for sell_listing in sell_listings:
+        sell_listing.image_urls = generate_image_urls(
+            image_ids=sell_listing.image_urls, request=request
+        )
+
+    log.info(f"{request.url.path} returning {sell_listings}")
+
+    return sell_listings
+
+
+@horse_selling_service_router.put(
+    path="/update-sell-listing/{horse_selling_service_id}"
+)
+def update_horse_renting_service_listings(
+    request: Request,
+    payload: Annotated[UpdateHorseForSellServiceListingValidator, Depends()],
+):
+
+    horse_selling_service_id = payload.horse_selling_service_id
+    update_details = payload.update_details
+    horse_id = payload.service_details.horse_id
+
+    log.info(f"{request.url.path} invoked update_details {update_details}")
+
+    update_horse(horse_id=horse_id, update_details=update_details.model_dump())
+
+    update_horse_selling_service_details(
+        service_id=horse_selling_service_id,
+        update_details={
+            "price_sar": update_details.price,
+            "updated_at": update_details.updated_at,
+        },
+    )
+
+    return {"status": "OK"}
+
+
+@horse_selling_service_router.post(path="/enquire-for-a-horse-sell")
+def create_rent_enquiry(
+    request: Request, payload: Annotated[CreateSellEnquiryValidator, Depends()]
+) -> CreateSellEnquiryResponse:
+
+    user = payload.user
+    old_enquiry_details = payload.old_enquiry_details
+    enquiry_details = payload.enquiry_details
+    update_existing = payload.update_existing
+
+    log.info(
+        f"{request.url.path} invoked enquiry_details {enquiry_details}, old_enquiry_details {old_enquiry_details}"
+    )
+
+    if update_existing:
+        update_horse_selling_service_enquiry(
+            enquiry_id=old_enquiry_details.horse_selling_enquiry_id,
+            enquiry_details=enquiry_details,
+        )
+
+        return CreateSellEnquiryResponse(
+            horse_selling_enquiry_id=old_enquiry_details.enquiry_id
+        )
+
+    enquiry = HorseSellingServiceEnquiryInternal(
+        user_id=user.id,
+        horse_selling_service_id=enquiry_details.horse_selling_service_id,
+        message=enquiry_details.message,
+        date=enquiry_details.date,
+        duration=enquiry_details.duration,
+    )
+
+    enquiry_id = add_horse_selling_service_enquiry(enquiry_details=enquiry)
+
+    response = CreateSellEnquiryResponse(horse_selling_enquiry_id=enquiry_id)
+
+    log.info(f"{request.url.path} returning {response}")
+
+    return response
+
+
+@horse_selling_service_router.put(
+    path="/update-horse-sell-enquiry/{horse_selling_enquiry_id}"
+)
+def update_rent_enquiry(
+    request: Request, payload: Annotated[UpdateSellEnquiryValidator, Depends()]
+):
+
+    horse_sell_enquiry_id = payload.horse_sell_enquiry_id
+    enquiry_details = payload.enquiry_details
+
+    log.info(f"{request.url.path} invoked enquiry_details {enquiry_details}")
+
+    update_horse_selling_service_enquiry(
+        enquiry_id=horse_sell_enquiry_id, enquiry_details=enquiry_details
+    )
+
+    return {"status": "OK"}
+
+
+@horse_selling_service_router.get(
+    path="/get-horse-sell-enquiries", response_model=List[GetHorseSellEnquiry]
+)
+def get_rent_enquiries(
+    request: Request,
+    payload: Annotated[GetHorseSellEnquiryValidator, Depends()],
+):
+
+    is_admin = payload.is_admin
+    user = payload.user
+    log.info(f"{request.url.path} invoked")
+
+    log.info(f"user_id {user.id}")
+
+    if is_admin:
+        response = get_all_horse_sell_enquiries()
+    else:
+        response = get_selling_enquiry_by_user_id(user_id=user.id)
+
+    return response
---
+++ data/dbapis/horse_selling_service/read_queries.py
@@ -0,0 +1,245 @@
+from typing import List, Union
+
+from api.horses.models import GetHorseRentListing
+from data.db import (
+    convert_to_object_id,
+    get_horse_selling_enquiry_collection,
+    get_horse_selling_service_collection,
+)
+from logging_config import log
+from models.horse.horse_selling_service_internal import (
+    HorseSellingServiceEnquiryInternalWithID,
+    HorseSellingServiceInternalWithID,
+)
+
+selling_service_collection = get_horse_selling_service_collection()
+selling_enquiry_collection = get_horse_selling_enquiry_collection()
+
+
+def get_selling_service_details_by_service_id(
+    service_id: str,
+) -> HorseSellingServiceInternalWithID:
+    """based on service_id of horse selling service return the horse selling service details
+
+    Args:
+        service_id (str)
+
+    Returns:
+        HorseSellingServiceInternalWithID
+    """
+
+    log.info(
+        f"get_selling_service_details_by_service_id() invoked service_id {service_id}"
+    )
+
+    filter = {"_id": convert_to_object_id(service_id)}
+    response = selling_service_collection.find_one(filter=filter)
+
+    renting_service_details = HorseSellingServiceInternalWithID(**response)
+
+    log.info(
+        f"get_selling_service_details_by_service_id() returning {renting_service_details}"
+    )
+
+    return renting_service_details
+
+
+def get_horse_sell_listings(
+    user_id: str, own_listing: bool = False
+) -> List[GetHorseRentListing]:
+    """return horses sell listings
+
+    Args:
+        own_listing (bool, optional):  Defaults to False.
+    """
+
+    match = {
+        "$match": (
+            {"provider.provider_id": user_id}
+            if own_listing
+            else {"provider.provider_id": {"$ne": user_id}}
+        )
+    }
+
+    add_fields_provider = {
+        "$addFields": {
+            "provider_object_id": {"$toObjectId": "$provider.provider_id"},
+        }
+    }
+
+    add_fields_horses = {
+        "$addFields": {"horse_object_id": {"$toObjectId": "$horse_id"}}
+    }
+
+    lookup_users = {
+        "$lookup": {
+            "from": "users",
+            "localField": "provider_object_id",
+            "foreignField": "_id",
+            "as": "user",
+        }
+    }
+
+    unwind_users = {"$unwind": "$user"}
+
+    lookup_horses = {
+        "$lookup": {
+            "from": "horse",
+            "localField": "horse_object_id",
+            "foreignField": "_id",
+            "as": "horse",
+        }
+    }
+
+    unwind_horses = {"$unwind": "$horse"}
+
+    project = {
+        "$project": {
+            "horse_selling_service_id": "$_id",
+            "horse_id": "$horse_id",
+            "name": "$horse.name",
+            "year_of_birth": "$horse.year_of_birth",
+            "breed": "$horse.breed",
+            "size": "$horse.size",
+            "gender": "$horse.gender",
+            "description": "$horse.description",
+            "image_urls": "$images",
+            "price": "$price_sar",
+            "seller_information": {
+                "name": "$user.full_name",
+                "email_address": "$user.email_address",
+                "phone_no": "$user.phone_number",
+                "location": "$user.address",
+            },
+            "_id": False,
+        }
+    }
+
+    pipeline = [
+        match,
+        add_fields_provider,
+        add_fields_horses,
+        lookup_users,
+        unwind_users,
+        lookup_horses,
+        unwind_horses,
+        project,
+    ]
+
+    response = selling_service_collection.aggregate(pipeline=pipeline)
+
+    rent_listings = [GetHorseRentListing(**rent_listing) for rent_listing in response]
+
+    return rent_listings
+
+
+def get_selling_enquiry_details_by_user_and_selling_service_id(
+    user_id: str, selling_service_id: str
+) -> Union[HorseSellingServiceEnquiryInternalWithID, None]:
+    """return an enquiry if the enquiry exists based on user_id and selling_service_id
+
+    Args:
+        user_id (str)
+        selling_service_id (str)
+
+    Returns:
+        HorseSellingServiceEnquiryInternalWithID
+    """
+
+    log.info(
+        f"get_selling_enquiry_details_by_user_and_selling_service_id() invoked user_id {user_id}, selling_service_id {selling_service_id}"
+    )
+
+    filter = {"user_id": user_id, "horse_selling_service_id": selling_service_id}
+
+    response = selling_enquiry_collection.find_one(filter=filter)
+    if not response:
+        return None
+
+    enquiry = HorseSellingServiceEnquiryInternalWithID(**response)
+
+    log.info(
+        f"get_selling_enquiry_details_by_user_and_selling_service_id() returning enquiry {enquiry}"
+    )
+
+    return enquiry
+
+
+def get_selling_enquiry_details_by_enquiry_id(
+    enquiry_id: str,
+) -> Union[HorseSellingServiceEnquiryInternalWithID, None]:
+    """return an enquiry if the enquiry exists based on the enquiry id
+
+    Args:
+        enquiry_id (str): _description_
+
+    Returns:
+        Union[HorseSellingServiceEnquiryInternalWithID, None]
+    """
+
+    log.info(
+        f"get_selling_enquiry_details_by_enquiry_id() invoked enquiry_id {enquiry_id}"
+    )
+
+    filter = {"_id": convert_to_object_id(enquiry_id)}
+    response = selling_enquiry_collection.find_one(filter=filter)
+
+    if not response:
+        return None
+
+    enquiry = HorseSellingServiceEnquiryInternalWithID(**response)
+
+    log.info(f"get_selling_enquiry_details_by_enquiry_id() returning enquiry {enquiry}")
+
+    return enquiry
+
+
+def get_selling_enquiry_by_user_id(
+    user_id: str,
+) -> List[HorseSellingServiceEnquiryInternalWithID]:
+    """return all the horse sell enquiry made by the user
+
+    Args:
+        user_id (str): _description_
+
+    Returns:
+        List[HorseSellingServiceEnquiryInternalWithID]
+    """
+
+    log.info(f"get_selling_enquiry_by_user_id() called user_id {user_id}")
+
+    filter = {"user_id": user_id}
+    response = selling_enquiry_collection.find(filter=filter)
+
+    if not response:
+        return []
+
+    enquiries = [
+        HorseSellingServiceEnquiryInternalWithID(**enquiry) for enquiry in response
+    ]
+
+    log.info(f"get_selling_enquiry_by_user_id() returning {enquiries}")
+
+    return enquiries
+
+
+def get_all_horse_sell_enquiries() -> List[HorseSellingServiceEnquiryInternalWithID]:
+    """return all the horse sell enquiry
+    Returns:
+        List[HorseSellingServiceEnquiryInternalWithID]
+    """
+
+    log.info(f"get_all_horse_sell_enquiries()")
+
+    response = selling_enquiry_collection.find()
+
+    if not response:
+        return []
+
+    enquiries = [
+        HorseSellingServiceEnquiryInternalWithID(**enquiry) for enquiry in response
+    ]
+
+    log.info(f"get_all_horse_sell_enquiries() returning {enquiries}")
+
+    return enquiries

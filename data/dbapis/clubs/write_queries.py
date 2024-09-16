c1f84432a340f0cd01104670f3ac2d5d117ff0bc
Nilanjan <nilthedev@gmail.com>
2024-09-16 17:58:36 +0530

    club write query overhaul

--- data/dbapis/clubs/write_queries.py
+++ data/dbapis/clubs/write_queries.py
@@ -1,32 +1,30 @@
 from data.db import get_clubs_collection
-from fastapi import HTTPException
 from logging_config import log
 from models.clubs.clubs_internal import ClubInternal
-from fastapi import status
+from fastapi import HTTPException, status
 
 club_collection = get_clubs_collection()
 
 
-def save_club(new_club: ClubInternal) -> str:
+def save_club(new_club: ClubInternal) -> ClubInternal:
     """
         saves the new user in the database and returns the id
         :param new_club: ClubInternal
-        :returns: id
+        :returns: ClubInternal
     """
-    log.info(f"save_club invoked: {new_club}")
+    log.info(f"inside save_club(new_club={new_club})")
 
-    # Check if a club with the same name and city already exists
-    existing_club = club_collection.find_one({"email_address": new_club.email_address})
+    result = club_collection.insert_one(new_club.model_dump())
 
-    if existing_club is not None:
-        emsg = f"Club with name {new_club.name} on {new_club.address} already exists."
-        log.info(emsg)
+    if not result.acknowledged:
+        log.info("new club can't be inserted in the database, raising exception...")
         raise HTTPException(
-            status_code=status.HTTP_303_SEE_OTHER,
-            detail=emsg
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="new club can't be inserted in the database due to unknown reasons..."
         )
 
-    club_id = (club_collection.insert_one(new_club.model_dump())).inserted_id
-    retval = str(club_id)
-    log.info(f"new club created with id: {retval}")
-    return retval
+    log.info(f"new club has successfully been inserted (club_id={new_club.id})")
+
+    return new_club
+
+

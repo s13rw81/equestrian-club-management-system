997a8506c80b105663f21b26792b2874a531404d
Nilanjan <nilthedev@gmail.com>
2024-09-23 21:07:40 +0530

    implemented `update_club`

--- data/dbapis/clubs/write_queries.py
+++ data/dbapis/clubs/write_queries.py
@@ -1,8 +1,9 @@
 from data.db import get_clubs_collection
 from logging_config import log
-from models.clubs.clubs_internal import ClubInternal
+from models.clubs import ClubInternal, UpdateClubInternal
 from fastapi import HTTPException, status
 from decorators import atomic_transaction
+from . import find_club
 
 club_collection = get_clubs_collection()
 
@@ -30,4 +31,33 @@ def save_club(new_club: ClubInternal, session=None) -> ClubInternal:
 
     return new_club
 
+@atomic_transaction
+def update_club(update_club_data: UpdateClubInternal, session=None) -> ClubInternal:
+    log.info(f"inside update_club(update_club_data={update_club_data})")
+    club_database_id = update_club_data.id.hex
 
+    update_club_dict = update_club_data.model_dump(exclude={"id"}, exclude_unset=True)
+
+    update_filter = {"id": club_database_id}
+
+    result = club_collection.update_one(
+        update_filter,
+        {"$set": update_club_dict},
+        session=session
+    )
+
+    log.info(
+        f"club update executed, matched_count={result.matched_count}, "
+        f"modified_count={result.modified_count}"
+    )
+
+    if not result.modified_count:
+        log.info("could not update club due to unknown reasons, raising exception")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="club cannot be updated in the database due to unknown reasons"
+        )
+
+    updated_club = find_club(id=club_database_id, session=session)
+
+    return updated_club

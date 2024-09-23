afb88c1bc5e03592ced8ee4037720cc7068552f4
Nilanjan <nilthedev@gmail.com>
2024-09-23 11:54:19 +0530

    implemented new read query

--- data/dbapis/clubs/read_queries.py
+++ data/dbapis/clubs/read_queries.py
@@ -21,6 +21,12 @@ def get_club_with_services_and_trainer(club_id: str):
 def get_club_by_user_id_logic(user_id: str) -> ClubInternal | None:
     pass
 
+def get_club_count() -> int:
+    log.info("inside get_club_count()")
+    club_count = club_collection.count_documents({})
+
+    log.info(f"returning {club_count}")
+    return club_count
 
 def find_club(**kwargs) -> Optional[ClubInternal]:
     log.info(f"inside find_club({kwargs})")

925e19bbf07e23946c536dab3305f96685ca4eb1
Nilanjan <nilthedev@gmail.com>
2024-10-10 21:13:01 +0530

    modified validators

--- api/user/models/sign_up_user.py
+++ api/user/models/sign_up_user.py
@@ -1,70 +1,67 @@
-from models.user import UserRoles
-from pydantic import BaseModel, field_validator, Field, model_validator
+from pydantic import (
+    BaseModel,
+    field_validator,
+    Field,
+    constr,
+    EmailStr
+)
 from typing import Optional
-from typing_extensions import Self
 from models.user.enums import RidingStage, HorseOwnership, EquestrianDiscipline
 from validators.user import whether_user_exists
-from validators.regex_validators import is_valid_email
+import phonenumbers
+from logging_config import log
 
 
 class SignUpUser(BaseModel):
-    full_name: str
-    email_address: Optional[str] = None
-    phone_number: Optional[str] = None
+    full_name: constr(min_length=1, max_length=200)
+    email_address: EmailStr
+    phone_number: str
+    phone_otp: constr(min_length=6, max_length=6)
     password: str = Field(exclude=True)
     riding_stage: Optional[RidingStage] = None
     horse_ownership_status: Optional[HorseOwnership] = None
     equestrian_discipline: Optional[EquestrianDiscipline] = None
 
     @field_validator("email_address")
-    @classmethod
-    def email_address_validator(cls, email: Optional[str]) -> Optional[str]:
-
-        if email is None:
-            return email
-
-        if not is_valid_email(email=email):
-            raise ValueError("not a valid email address...")
+    def email_address_validator(cls, email):
 
         result = whether_user_exists(email=email)
 
         if result:
+            log.info("an user with the same email_address already exists, raising ValueError")
             raise ValueError("email already exists...")
 
         return email
 
     @field_validator("phone_number")
-    @classmethod
-    def email_validator(cls, phone_number: Optional[str]) -> Optional[str]:
+    def phone_number_validator(cls, phone_number):
 
-        if phone_number is None:
-            return phone_number
+        error = ValueError(f"invalid phone number (phone_number={phone_number})")
+
+        try:
+            parsed_phone_number = phonenumbers.parse(phone_number)
+        except phonenumbers.NumberParseException:
+            log.info(f"failed to parse phone number, raising error (phone_number={phone_number})")
+            raise error
+
+        if not phonenumbers.is_valid_number(parsed_phone_number):
+            log.info(f"phone number is not valid, raising error (phone_number={phone_number})")
+            raise error
 
         result = whether_user_exists(phone=phone_number)
 
         if result:
+            log.info("an user with the same phone_number already exists, raising ValueError")
             raise ValueError("phone_number already exists...")
 
-        return phone_number
+        return phonenumbers.format_number(parsed_phone_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
 
     @field_validator("password")
-    @classmethod
     def password_validator(cls, password: str) -> str:
         password = password.strip()
 
         if len(password) < 6:
+            log.info("password length is less than 6 characters, raising ValueError")
             raise ValueError("password should be more than 5 characters...")
 
         return password
-
-    @model_validator(mode="after")
-    def check_whether_email_and_phone_both_are_present(self) -> Self:
-        # email_address and phone_number both are present
-        if self.email_address and self.phone_number:
-            raise ValueError("either email_address or phone_number should be present not both!")
-
-        # neither email_address nor phone_number is present
-        if not (self.email_address or self.phone_number):
-            raise ValueError("either email_address or phone_number must be present!")
-
-        return self

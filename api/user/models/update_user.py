67d2324f70e6a80b7bec31d69f10d9d780444050
Nilanjan <nilthedev@gmail.com>
2024-10-15 17:58:58 +0530

    basic refactoring

--- api/user/models/update_user.py
+++ api/user/models/update_user.py
@@ -4,7 +4,7 @@ from models.user import RidingStage, HorseOwnership, EquestrianDiscipline
 
 
 class UpdateUser(BaseModel):
-    full_name: Optional[str] = None
+    full_name: str = None
     riding_stage: Optional[RidingStage] = None
     horse_ownership_status: Optional[HorseOwnership] = None
     equestrian_discipline: Optional[EquestrianDiscipline] = None

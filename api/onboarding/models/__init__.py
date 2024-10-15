47564a7aa4daa6705efc6d962f235f46f4076279
Nilanjan <nilthedev@gmail.com>
2024-10-15 17:46:12 +0530

    init files

--- api/onboarding/models/__init__.py
+++ api/onboarding/models/__init__.py
@@ -1,4 +1,5 @@
 from api.onboarding.models.add_club_model import *
 from api.onboarding.models.add_logistics_company_model import *
-from api.onboarding.models.trainer import *
+from api.onboarding.models.create_trainer_dto import *
 from api.onboarding.models.update_club_model import *
+from .create_trainer_dto import CreateTrainerDTO
---
+++ data/dbapis/trainers/__init__.py
@@ -0,0 +1,2 @@
+from .read_queries import find_trainer, find_many_trainers
+from .write_quries import save_trainer, update_trainer

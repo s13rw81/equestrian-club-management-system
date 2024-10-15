c0f5d6d98d2099cfdbc84b3d96bcc07b21179037
Nilanjan <nilthedev@gmail.com>
2024-10-15 17:50:00 +0530

    minor changes

--- models/clubs/clubs_internal.py
+++ models/clubs/clubs_internal.py
@@ -1,6 +1,4 @@
-from functools import lru_cache
 from pydantic import BaseModel, field_serializer
-from models.ratings.club_ratings_internal import ClubRatingsInternal
 from ..common_base import CommonBase
 from ..location.location_internal import LocationInternal
 from .enums.verification_status import VerificationStatus
@@ -32,28 +30,3 @@ class ClubInternal(CommonBase):
     @field_serializer("verification_status")
     def verification_status_serializer(self, verification_status):
         return verification_status.value
-
-
-
-
-
-
-
-
-@lru_cache(maxsize=64)
-def calculate_average_rating(club_id: str):
-    # TODO: review this logic
-    # TODO: can anonymous user add rating ?
-    # TODO: can non subscribing user add rating ?
-
-    # TODO: move this to logic then dbapi
-    ratings_cursor = ClubRatingsInternal.find({"club_id": club_id})
-    total = 0
-    count = 0
-    for rating in ratings_cursor:
-        total += rating['rating']
-        count += 1
-    if count > 0:
-        return total / count
-    else:
-        return None

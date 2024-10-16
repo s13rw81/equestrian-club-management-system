b07105faba6994975ac2ccf0e73f5bbdfc411446
Nilanjan <nilthedev@gmail.com>
2024-10-16 11:46:58 +0530

    bug fixes

--- api/onboarding/clubs.py
+++ api/onboarding/clubs.py
@@ -1,26 +1,21 @@
 from typing import Annotated
-from api.onboarding.models import CreateTrainerDTO
-from api.onboarding.onboarding_router import onboarding_api_router
 from data.dbapis.clubs import get_club_count
 from fastapi import Depends
 from logging_config import log
 from logic.onboarding.clubs import create_club as create_club_logic
-from models.user import UserInternal, UserRoles
+from models.user import UserInternal
+from models.user.enums import UserRoles
 from models.clubs import ClubInternal, ClubUser
-from role_based_access_control import RoleBasedAccessControl
 from models.http_responses import Success
-
+from .models import CreateClubRequest
+from role_based_access_control import RoleBasedAccessControl
+from . import onboarding_api_router
 
 @onboarding_api_router.post("/create-club")
 async def create_club(
-        create_club_request: CreateTrainerDTO,
+        create_club_request: CreateClubRequest,
         user: Annotated[UserInternal, Depends(RoleBasedAccessControl({UserRoles.USER}))]
 ):
-    """
-    :param user: user invoking the api
-    :param create_club_request: instance of CreateClub dto
-    :return: instance of str, id of new club created
-    """
     log.info(f"/create-club invoked (create_club_request={create_club_request}, user_id={user.id})")
 
     existing_club_count = get_club_count()
@@ -28,7 +23,7 @@ async def create_club(
     club = ClubInternal(
         created_by=user.id,
         users=[
-            ClubUser(user_id=user.id)
+            ClubUser(user_id=str(user.id))
         ],
         platform_id=f"khayyal_{create_club_request.club_id}_{existing_club_count + 1}",
         **create_club_request.model_dump()

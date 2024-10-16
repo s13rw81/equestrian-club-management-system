7090d87c493aea1c8895e7855802d9a89aefa154
Nilanjan <nilthedev@gmail.com>
2024-10-16 11:46:18 +0530

    removed outdated code

--- data/dbapis/clubs/delete_queries.py
+++
@@ -1,9 +0,0 @@
-from bson import ObjectId
-from data.db import get_clubs_collection
-
-clubs_collection = get_clubs_collection()
-
-
-def delete_club_by_id_logic(club_id: str) -> str:
-    result = clubs_collection.delete_one({"_id": ObjectId(club_id)})
-    return result.deleted_count == 1

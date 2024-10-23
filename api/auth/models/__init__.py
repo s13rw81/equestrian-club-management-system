209f531b105b50ef92c76b04cd4952d1096df313
Nilanjan <nilthedev@gmail.com>
2024-10-24 01:46:12 +0530

    init files

--- api/auth/models/__init__.py
+++ api/auth/models/__init__.py
@@ -1,3 +1,5 @@
 from .token import Token
 from .generate_sign_up_otp_dto import GenerateSignUpOtpDTO
 from .generate_reset_password_otp_dto import GenerateResetPasswordOtpDTO
+from .reset_password_dto import ResetPasswordDTO
+from .reset_password_verify_otp_dto import ResetPasswordVerifyOtpDTO
--- models/user/enums/__init__.py
+++ models/user/enums/__init__.py
@@ -2,3 +2,4 @@ from .equestrian_discipline import EquestrianDiscipline
 from .horse_ownership import HorseOwnership
 from .riding_stage import RidingStage
 from .user_roles import UserRoles
+from .gender import Gender

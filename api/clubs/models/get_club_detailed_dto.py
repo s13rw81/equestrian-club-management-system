e3aad8c43858bd27f428b67db09dbe8c54e6b0a7
Nilanjan <nilthedev@gmail.com>
2024-10-30 00:56:32 +0530

    implemented `GetClubDetailedDTO`

---
+++ api/clubs/models/get_club_detailed_dto.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, field_serializer
+from typing import Optional
+from models.clubs.enums import VerificationStatus
+from uuid import UUID
+
+class GetLocation(BaseModel):
+    lat: str
+    long: str
+
+class GetClubDetailedDTO(BaseModel):
+    id: UUID
+    name: str
+    owner_name: str
+    phone_number: str
+    email_id: str
+    commercial_registration: str
+    club_id: str
+    iban: str
+    location: GetLocation
+    platform_id: str
+    logo: Optional[str] = None
+    images: Optional[list[str]] = None
+    verification_status: VerificationStatus
+    approx_price: Optional[str] = None
+    average_rating: Optional[str] = None
+    services: Optional[list[str]] = None
+    reviews: Optional[list[str]] = None
+
+    @field_serializer("verification_status")
+    def verification_status_serializer(self, verification_status):
+        return verification_status.value
+
+    @field_serializer("id")
+    def serialize_uuids(self, value):
+        if not value:
+            return
+
+        return str(value)

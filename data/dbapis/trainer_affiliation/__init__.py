bb92cc05a6b957d68351a1eb6ce2d0adca3347a6
Nilanjan <nilthedev@gmail.com>
2024-11-02 23:39:05 +0530

    init file

--- data/dbapis/trainer_affiliation/__init__.py
+++ data/dbapis/trainer_affiliation/__init__.py
@@ -1,2 +1,2 @@
-from .read_queries import find_trainer_affiliation
+from .read_queries import find_trainer_affiliation, find_many_trainer_affiliations
 from .write_queries import save_trainer_affiliation

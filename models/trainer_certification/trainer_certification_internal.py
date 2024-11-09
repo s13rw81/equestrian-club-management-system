3c10bce0d9387fd9facde372c40de2aae5636e70
Nilanjan <nilthedev@gmail.com>
2024-11-10 02:01:54 +0530

    implemented `TrainerCertificationInternal`

---
+++ models/trainer_certification/trainer_certification_internal.py
@@ -0,0 +1,14 @@
+from ..common_base import CommonBase
+from typing import Optional
+
+
+class TrainerCertificationInternal(CommonBase):
+    # user-fields
+    name: str
+    number: str
+
+    # system-fields
+    # the id of the associated image
+    image_id: Optional[str] = None
+    # the id of the corresponding trainer
+    trainer_id: str

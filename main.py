cc9bbef3730aa813bce8f288c6a6e34b15732365
Nilanjan <nilthedev@gmail.com>
2024-11-21 02:37:59 +0530

    upgraded fastapi

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ async def root(request: Request):
     log.info(
         f"method: {request.method}, headers: {request.headers}, client: {request.client}"
     )
-    return {"status": "Hello World"}
     return RedirectResponse(url="/docs")
 
 

0d075c8d540401c53148f57919ef4191b264bdc1
Nilanjan <nilthedev@gmail.com>
2024-11-23 14:46:06 +0530

    minor fixes

--- logic/trainer_affiliation/trainer_affiliation_get_query_with_pagination.py
+++ logic/trainer_affiliation/trainer_affiliation_get_query_with_pagination.py
@@ -1,6 +1,6 @@
 from models.trainer_affiliation import TrainerAffiliationDetailedInternal
 from ..generic_get_query_with_pagination import generic_get_query_with_pagination_logic
-from data.db import get_trainer_affiliation_collection
+from data.db import get_trainer_affiliations_collection
 from decorators import atomic_transaction
 from typing import Optional
 from logging_config import log
@@ -18,7 +18,7 @@ def trainer_affiliation_get_query_with_pagination(
              f"f={f}, s={s}, page_no={page_no}, page_size={page_size})")
 
     result = generic_get_query_with_pagination_logic(
-        primary_collection=get_trainer_affiliation_collection(),
+        primary_collection=get_trainer_affiliations_collection(),
         final_output_model=TrainerAffiliationDetailedInternal,
         f=f, s=s, page_no=page_no, page_size=page_size, session=session
     )

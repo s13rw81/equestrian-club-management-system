0aa148abf09ec5a365be6a33d69fecb2ffdf39c1
Nilanjan <nilthedev@gmail.com>
2024-11-23 14:47:27 +0530

    implemented `GetTrainerCertificationDTO`

---
+++ api/trainers/models/get_trainer_detailed_dto.py
@@ -0,0 +1,26 @@
+from . import GetTrainerDTO
+
+from pydantic import BaseModel, field_validator
+from typing import Optional
+from utils.image_management import generate_image_url
+
+class GetTrainerCertificationDTO(BaseModel):
+    name: str
+    number: str
+    image: Optional[str] = None
+    trainer_id: str
+
+    @field_validator("image")
+    def convert_image_id_to_url(cls, image):
+        if not image:
+            return image
+        return generate_image_url(image_id=image)
+
+class GetTrainerSpecializationDTO(BaseModel):
+    name: str
+    years_of_experience: int
+    trainer_id: str
+
+class GetTrainerDetailedDTO(GetTrainerDTO):
+    certifications: list[GetTrainerCertificationDTO] = []
+    specializations: list[GetTrainerSpecializationDTO] = []

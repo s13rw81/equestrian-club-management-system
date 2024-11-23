8e2005ec1a51f1d6aab46739017753ded0937edb
Nilanjan <nilthedev@gmail.com>
2024-11-23 14:46:47 +0530

    amended `CreateTrainerDTO`

--- api/onboarding/models/create_trainer_dto.py
+++ api/onboarding/models/create_trainer_dto.py
@@ -1,20 +1,39 @@
-from pydantic import BaseModel, constr, Field, field_validator
-from data.dbapis.clubs import find_club
+from pydantic import BaseModel, constr, field_validator, Field
+from data.dbapis.trainer_affiliation import find_trainer_affiliation
 from logging_config import log
+from models.trainers.enums import AvailableService, WeekDay, TimeSlot
+
+class CreateTrainerCertificationDTO(BaseModel):
+    name: constr(min_length=1, max_length=200)
+    number: constr(min_length=1, max_length=200)
+
+class CreateTrainerSpecializationDTO(BaseModel):
+    name: constr(min_length=1, max_length=200)
+    years_of_experience: int = Field(gt=0)
 
 class CreateTrainerDTO(BaseModel):
     full_name: constr(min_length=1, max_length=200)
-    club_affiliation: str = Field(..., description="associated club_id")
+    bio: constr(min_length=1, max_length=1000)
+    club_affiliation_number: str
+    available_services: list[AvailableService]
+    availability: list[WeekDay]
+    preferred_time_slot: TimeSlot
+    # using `default_factory` instead of `default` to preserve the nested key names in the openapi schema
+    certifications: list[CreateTrainerCertificationDTO] = Field(default_factory=list)
+    specializations: list[CreateTrainerSpecializationDTO] = Field(default_factory=list)
 
-    @field_validator("club_affiliation")
-    def validate_club_affiliation(cls, club_affiliation):
-        log.info(f"inside validate_club_affiliation(club_affiliation={club_affiliation})")
+    @field_validator("full_name")
+    def full_name_capitalize(cls, full_name):
+        return " ".join([item.capitalize() for item in full_name.split()])
 
-        club = find_club(id=club_affiliation)
+    @field_validator("club_affiliation_number")
+    def validate_club_affiliation_number(cls, club_affiliation_number):
+        result = find_trainer_affiliation(id=club_affiliation_number)
 
-        if not club:
-            log.info(f"invalid club_id = {club_affiliation}, raising ValueError...")
-            raise ValueError(f"invalid club_id={club_affiliation}")
+        if not result:
+            log.info(f"invalid club_affiliation_number: {club_affiliation_number}, "
+                     "raising ValueError...")
 
-        return club_affiliation
-
+            raise ValueError(f"invalid club_affiliation_number: {club_affiliation_number}, "
+                             "raising ValueError...")
+        return club_affiliation_number

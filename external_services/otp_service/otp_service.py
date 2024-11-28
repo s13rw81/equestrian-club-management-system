39503179d9ddc46781fb26b6aa1030cf3efd0325
nilthedev <nilthedev@gmail.com>
2024-11-29 00:52:11 +0530

    implemented `send_otp_external`

--- external_services/otp_service/otp_service.py
+++ external_services/otp_service/otp_service.py
@@ -2,12 +2,57 @@ from logging_config import log
 from fastapi.exceptions import HTTPException
 from fastapi import status
 import random
+from config import (
+    OTP_SERVICE_ACTIVATED,
+    OTP_SERVICE_API_URL,
+    OTP_SERVICE_USERNAME,
+    OTP_SERVICE_USER_SENDER,
+    OTP_SERVICE_API_KEY
+)
+import requests
+
+def send_otp_external(otp, phone_number):
+    log.info(f"inside send_otp_external(otp={otp}, phone_number={phone_number})")
+
+    request_data = {
+      "userName": OTP_SERVICE_USERNAME,
+      "numbers": phone_number,
+      "userSender": OTP_SERVICE_USER_SENDER,
+      "apiKey": OTP_SERVICE_API_KEY,
+      "msg": f"OTP FOR KHAYYAL: {otp}"
+    }
+
+    request_headers = {
+        'Content-Type': 'application/json'
+    }
+
+    request_url = OTP_SERVICE_API_URL + "/sendsms.php"
+
+    response = requests.post(request_url, json=request_data, headers=request_headers)
+
+    log.info("request to external otp service has been completed; "
+             f"status_code={response.status_code}, "
+             f"headers={response.headers}, "
+             f"data={response.json()}")
+
+    if response.status_code != 200:
+        log.info("failed to send otp, raising HTTPException...")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="failed to send otp..."
+        )
+
+    log.info("OTP has been successfully sent, returning True...")
+    return True
 
 
 def generate_otp() -> str:
-    # TODO: Remove it, this is only a temporary fix
-    return "123654"
-    # return str(random.randrange(100000, 1000000))
+    log.info("inside generate_otp()")
+    # generate a random OTP if otp service is activated, otherwise return default OTP 123654
+    retval = str(random.randrange(100000, 1000000)) if OTP_SERVICE_ACTIVATED else "123654"
+
+    log.info(f"returning {retval}")
+    return retval
 
 
 def send_otp_email(email_address: str, existing_otp: str = None) -> str:
@@ -23,18 +68,12 @@ def send_otp_email(email_address: str, existing_otp: str = None) -> str:
     otp = generate_otp() if not existing_otp else existing_otp
     log.debug(f"GENERATED_OTP={otp}")
 
-    # result = send_dynamic_email(
-    #     to_email_address=email_address,
-    #     first_name=first_name,
-    #     last_name=last_name,
-    #     otp=otp
-    # )
-    #
-    # if not result:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-    #         detail="could not send otp email"
-    #     )
+    if OTP_SERVICE_ACTIVATED:
+        log.info("email OTP service has not yet been implemented; raising HTTPException...")
+        raise HTTPException(
+            status_code=status.HTTP_501_NOT_IMPLEMENTED,
+            detail="email OTP service has not yet been implemented..."
+        )
 
     return otp
 
@@ -52,17 +91,7 @@ def send_otp_phone(phone_number: str, existing_otp: str = None) -> str:
     otp = generate_otp() if not existing_otp else existing_otp
     log.debug(f"GENERATED_OTP={otp}")
 
-    # result = send_dynamic_email(
-    #     to_email_address=email_address,
-    #     first_name=first_name,
-    #     last_name=last_name,
-    #     otp=otp
-    # )
-    #
-    # if not result:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-    #         detail="could not send otp email"
-    #     )
+    if OTP_SERVICE_ACTIVATED:
+        send_otp_external(otp=otp, phone_number=phone_number)
 
     return otp
